aa3fdd190473b851a216070c1a737738ba50764f
Camila Carrasco <69226192+Camicb@users.noreply.github.com>
2022-06-25 22:21:12 -0400

    Create devops_bootcamp.py

---
+++ devops_bootcamp.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre:")
+edad = input("Ingrese su edad (en números):")
+edad_año_pasado = int(edad) -1
+edad_año_siguiente = int(edad) +1
+
+print(f"{nombre}, el año pasado tenías {edad_año_pasado} años y el próximo año cumplirás {edad_año_siguiente} años")
